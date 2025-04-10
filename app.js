9f8a020414ea21a3dfca2b1dd7c00eb13c92d7a6
ah3863 <ah3863@nyu.edu>
2025-04-10 17:54:23 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
-mapboxgl.accessToken = 'pk.eyJ1IjoiYWgzODYzIiwiYSI6ImNtOWJud3p3YjBoanQyc29xeHVsbDE4YmEifQ.YNU_qv33YYjYGBtGimY-7w'; // Replace with your Mapbox token
+mapboxgl.accessToken = 'pk.eyJ1IjoiYWgzODYzIiwiYSI6ImNtOWJ2ZzJoMDBreHIyanBuYmFvb3lvam0ifQ.Zeg12vyxmHuz86vEocSGdw'; // Replace with your Mapbox token
 var map = new mapboxgl.Map({
-  container: 'mapcontainer', // The ID of the div where the map will render
+  container: 'map-container', // The ID of the div where the map will render
   style: 'mapbox://styles/mapbox/streets-v11', // Style of the map
   center: [-74.5, 40], // Initial position [longitude, latitude]
   zoom: 9, // Initial zoom level
