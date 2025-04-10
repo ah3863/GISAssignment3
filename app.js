21294ef911f4267d0fc16a656d77c91db371e3e0
ah3863 <ah3863@nyu.edu>
2025-04-10 17:09:37 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,139 +1,48 @@
-mapboxgl.accessToken = 'pk.eyJ1IjoiYWgzODYzIiwiYSI6ImNtOWJud3p3YjBoanQyc29xeHVsbDE4YmEifQ.YNU_qv33YYjYGBtGimY-7w'; // Replace with your Mapbox token
-var map = new mapboxgl.Map({
-  container: 'map', // The ID of the div where the map will render
-  style: 'mapbox://styles/mapbox/streets-v11', // Style of the map
-  center: [-74.5, 40], // Initial position [longitude, latitude]
-  zoom: 9, // Initial zoom level
+mapboxgl.accessToken = 'pk.eyJ1IjoiYWgzODYzIiwiYSI6ImNtOWJud3p3YjBoanQyc29xeHVsbDE4YmEifQ.YNU_qv33YYjYGBtGimY-7w';
+
+const map = new mapboxgl.Map({
+    container: 'map', // ID of the div where the map renders
+    style: 'mapbox://styles/mapbox/streets-v11',
+    center: [-73.97657, 40.75869], // Starting position [longitude, latitude]
+    zoom: 12 // Starting zoom level
 });
 
-const mapOptions = {
-    container: 'map-container', // container ID
-    center: [-73.99432, 40.71103], // starting position [lng, lat]. Note that lat must be set between -90 and 90
-    zoom: 10.92, // starting zoom
-    bearing: 29,
-    style: 'mapbox://styles/mapbox/streets-v11'
-    // hash: true
-}
+const SATCData = [
+    {
+      "location_name": "Carrie Bradshaw’s apartment (theoretical)",
+      "character": "Carrie",
+      "episode": "Season: 1. Episode: 1 ",
+      "description": "Overflowing with Vogue magazines and piles of clothing...",
+      "longitude": [-73.95817, 40.76961]
+    },
+    {
+      "location_name": "Carrie Bradshaw’s apartment (filming location)",
+      "character": "Carrie",
+      "episode": "Season: 1. Episode: 1 ",
+      "description": "The iconic staircase where Carrie was often spotted...",
+      "longitude": [-74.00376, 40.73535]
+    },
+    // Add more locations...
+];
 
-const coolMap = new mapboxgl.Map(mapOptions);
+// Loop through each SATC location and add a marker with a popup
+SATCData.forEach((location) => {
+    const popup = new mapboxgl.Popup({ offset: 36 }).setHTML(`
+        <h3>${location.location_name}</h3>
+        <p><strong>Character:</strong> ${location.character}</p>
+        <p><strong>Episode:</strong> ${location.episode}</p>
+        <p>${location.description}</p>
+    `);
 
+    let markerColor = '#ccc'; // Default marker color
 
-const markerOptions = {
-    color: '#57068C',
-}
+    if (location.character === 'Carrie') markerColor = 'red';
+    if (location.character === 'Miranda') markerColor = 'green';
+    if (location.character === 'Charlotte') markerColor = 'blue';
+    if (location.character === 'Samantha') markerColor = 'yellow';
 
-
-create the popup
-const popup = new mapboxgl.Popup({
-     offset: 36,
- }).setText(
-     'These are iconic landmarks in NYC from Sex and the City!'
- );
-
-Carrie Bradshaw’s apartment (theoretical)
- const marker1 = new mapboxgl.Marker(markerOptions)
-    .setLngLat([-73.95817, 40.76961])
-    .setPopup(popup)
-    .addTo(coolMap);
-
-Carrie Bradshaw’s apartment (filming location)
- const marker2 = new mapboxgl.Marker(markerOptions)
-     .setLngLat([-74.00376, 40.73535])
-     .addTo(coolMap);
-
-Louis K. Meisel Gallery
- const marker3 = new mapboxgl.Marker(markerOptions)
-     .setLngLat([-74.00052, 40.72568])
-     .addTo(coolMap);
-
-The Bus Stop
- const marker4 = new mapboxgl.Marker(markerOptions)
-     .setLngLat([-74.00686, 40.73510])
-     .addTo(coolMap);
-
-St. Patrick’s Cathedral
- const marker5 = new mapboxgl.Marker(markerOptions)
-     .setLngLat([-73.97657, 40.75869])
-     .addTo(coolMap);
-
-Yankee Stadium
- const marker6 = new mapboxgl.Marker(markerOptions)
-     .setLngLat([-73.92764, 40.82925])
-     .addTo(coolMap);
-
-The Plaza Hotel
- const marker7 = new mapboxgl.Marker(markerOptions)
-     .setLngLat([-73.97388, 40.76445])
-     .addTo(coolMap);
-
-Staten Island Ferry
- const marker8 = new mapboxgl.Marker(markerOptions)
-     .setLngLat([-74.01305, 40.70093])
-     .addTo(coolMap);
-
-Magnolia Bakery
-const marker9 = new mapboxgl.Marker(markerOptions)
-     .setLngLat([-74.00498, 40.73589])
-     .addTo(coolMap);
-
-Grace Episcopal Church
-const marker10 = new mapboxgl.Marker(markerOptions)
-     .setLngLat([-73.99097, 40.73201])
-     .addTo(coolMap);
-
-The Central Park Loeb Boathouse
-const marker11 = new mapboxgl.Marker(markerOptions)
-     .setLngLat([-73.96879, 40.77534])
-     .addTo(coolMap);
-
-Soho House New York
-const marker12 = new mapboxgl.Marker(markerOptions)
-     .setLngLat([-74.00588, 40.74062])
-     .addTo(coolMap);
-
-Jefferson Market Garden
-const marker13 = new mapboxgl.Marker(markerOptions)
-     .setLngLat([-73.99953, 40.73445])
-     .addTo(coolMap);
-
-New York Public Library
-const marker14 = new mapboxgl.Marker(markerOptions)
-     .setLngLat([-73.98209, 40.75335])
-     .addTo(coolMap);
-
-// now let's make a markers for our favorite SATC places
-
-SATCData.forEach((record) => {
-    const popup = new mapboxgl.Popup({
-        offset: 36,
-    }).setText(
-        `${description}.`
-    );
-
-    let programColor = '#ccc';
-
-    if (record.character === 'Miranda') {
-        programColor = 'green'
-    }
-
-    if (record.character === 'Carrie') {
-        programColor = 'red'
-    }
-
-    if (record.character === 'Charlotte') {
-        programColor = 'blue'
-    }
-
-    if (record.character === 'Samantha') {
-        programColor = 'yellow'
-    }
-
-
-    new mapboxgl.Marker({
-        scale: 0.8,
-        color: programColor
-    })
-        .setLngLat([record.longitude, record.latitude])
-        .setPopup(popup)
-        .addTo(coolMap)
-})
+    new mapboxgl.Marker({ color: markerColor })
+        .setLngLat(location.longitude) // Longitude, Latitude
+        .setPopup(popup) // Attach popup
+        .addTo(map);
+});
